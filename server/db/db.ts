fc6e964e0c46a01a5d78e9527f757cfde3ea07ad
Shrena Patel <shrena.p@hotmail.co.uk>
2023-06-05 09:47:34 +1200

    get and add posts working

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,3 +1,26 @@
+import { Post } from '../../models/post';
 import connection from './connection'
 
-export function getAllPosts(db = connection) {}
+export function getAllPosts(db = connection): Promise<Post[]> {
+  return db('posts').select(
+    'id',
+    'title',
+    'date_created as dateCreated',
+    'text'
+   )
+}
+
+export function getPost(id: number, db = connection): Promise<Post> {
+  return db('posts')
+  .select('id', 'title', 'date_created as dateCreated', 'text')
+  .where('id', id)
+  .first()
+}
+
+export function addPost(post: {title: string, text: string}, db = connection): Promise<Post> {
+  return db('posts')
+    .insert({...post, date_created: Date.now() })
+    .then((id) => {
+      return getPost(id[0])
+    })
+}
--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -1,5 +1,28 @@
 import express from 'express'
+import { Post } from '../../models/post'
+import { addPost, getAllPosts, getPost } from '../db/db'
 
 const router = express.Router()
+
+router.get('/', (req, res) => {
+  return getAllPosts().then((posts: Post[]) => {
+      res.json(posts)
+  })
+  .catch((err: Error) => res.status(500).json(err.message))
+})
+
+router.post('/', (req, res) => {
+  const post = req.body
+  return addPost(post)
+      // .then((postIdArr) => getPost(postIdArr[0]))
+      // .then((oneFullPost) => res.json(oneFullPost))
+      // .catch((err) => res.status(500).json(err.message))
+      // OR:
+      
+      .then((post) =>  {
+          res.json(post)
+      })
+      .catch((err) => res.status(500).json(err.message))
+})
 
 export default router
